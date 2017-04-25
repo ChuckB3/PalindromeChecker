ce16f75d5d54d2a8ffd8088dd3d5ab05d75d0c41
ChuckB3 <cbrumbaugh3@gmail.com>
2017-04-25 16:40:52 -0400

    Import Project

---
+++ PalindromeChecker/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PalindromeChecker
+{
+    
+    public partial class PalindromeChecker : Form
+    {
+        private String startStr;
+        private String revStr;
+        public PalindromeChecker()
+        {
+            InitializeComponent();
+        }
+
+        private String reverseString(String s)
+        {
+            if (s == null) return null;
+
+            char[] a = s.ToCharArray();
+            Array.Reverse(a);
+            return new string(a);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startStr = strBox.Text;
+            revStr = reverseString(startStr);
+            
+            if (revStr.ToUpper()==startStr.ToUpper())
+            {
+                MessageBox.Show("The reverse of the string is " + revStr.ToLower()+".\n"+startStr+" is a palindrome!","Result");
+            }
+            else
+            {
+                MessageBox.Show("The reverse of the string is " + revStr.ToLower() + ".\n" + startStr + " is not a palindrome.","Result");
+            }
+        }
+    }
+}
---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace PalindromeChecker
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PalindromeChecker());
+        }
+    }
+}
